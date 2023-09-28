dc3858000452d5998665673458da81273660e9e1
ShivanshSinghnnovative <shivansh.singh@innovative.in>
2023-09-28 15:14:14 +0530

    step4code

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="allCard">
-    <cardShow v-for="pokemon in pokemons" :key="pokemon.id" @click="handleCardClick(pokemon.id)">
+    <cardShow v-for="pokemon in pokemons" :key="pokemon.id" @click="pokemonEvolution(pokemon.id)">
+      <template v-slot:title>
+        {{ pokemon.name }} # {{ pokemon.id }}
+      </template>
+      <template v-slot:content>
+        <img :src="pokemon.img" />
+      </template>
+      <template v-slot:footer>
+        <div v-for="feature in pokemon.types" :key="feature.slot">
+          {{ feature.type.name }}
+        </div>
+      </template>
+    </cardShow>
+  </div>
+  <div class="allCard">
+    <cardShow v-for="pokemon in evolution" :key="pokemon.id">
       <template v-slot:title>
         {{ pokemon.name }} # {{ pokemon.id }}
       </template>
@@ -21,10 +36,14 @@ import { onMounted, ref } from 'vue'
 import cardShow from './components/cardShow.vue'
 const pokemonAPI = ref(process.env.VUE_APP_POKEMON_API_URL);
 
+
 let pokemons = ref('');
+let evolution = ref('');
+
 onMounted(() => {
   togetData();
 })
+
 const togetData = async () => {
   try {
     let promises = [dataPokemon(1), dataPokemon(4), dataPokemon(7)];
@@ -38,6 +57,23 @@ const togetData = async () => {
   } catch (err) {
     console.error(err);
   }
+}
+
+const pokemonEvolution = (id) => {
+  const evolutions = [dataPokemon(id + 1), dataPokemon(id + 2)];
+
+  Promise.all(evolutions)
+    .then((returned) => {
+      evolution.value = returned.map((data) => ({
+        id: data.id,
+        name: data.name,
+        img: data.img,
+        types: data.types,
+      }));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
 const dataPokemon = async (key) => {
